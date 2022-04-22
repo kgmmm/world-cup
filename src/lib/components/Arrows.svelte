b283958d408b30eb49220e2b2aa609e431253c7b
kgmmm <kgmofficial0@gmail.com>
2022-04-22 22:24:44 +0100

    arrows to flick forward and backward through Progress with progress indicator

---
+++ src/lib/components/Arrows.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { progress } from "$lib/stores";
+  import IconButton from "./IconButton.svelte";
+
+  function stepForward() {
+    $progress[1] = $progress[1] + 1;
+  }
+  function stepBackward() {
+    $progress[1] = $progress[1] - 1;
+  }
+</script>
+
+<div>
+  <IconButton type="leftArrow" on:Click={stepBackward} />
+  <IconButton type="rightArrowAccent" on:Click={stepForward} />
+  <p>{$progress[1]}/{$progress[0].length - 2}</p>
+</div>
+
+<style>
+  div {
+    width: 100%;
+    max-width: 475px;
+    padding-inline: 1rem;
+    display: grid;
+    grid-auto-flow: column;
+    place-items: center;
+    position: relative;
+  }
+
+  p {
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: rgba(0, 0, 0, 50%);
+  }
+</style>
