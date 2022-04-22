b1bdf57384bc8d08c36a1043227cc943c51fe518
kgmmm <kgmofficial0@gmail.com>
2022-04-22 22:26:41 +0100

    displaying Splash on page load

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { progress } from "$lib/stores";
+  import Splash from "$lib/components/Splash.svelte";
+
+  function stepForward() {
+    $progress[1] = $progress[1] + 1;
+  }
+  function stepBackward() {
+    $progress[1] = $progress[1] - 1;
+  }
+</script>
+
+{#if $progress[1] === 0}
+  <Splash on:Click={stepForward} />
+{/if}
