4da14c3ba04f5fa91e89da9f0851c7e187f6cd74
kgmmm <kgmofficial0@gmail.com>
2022-04-22 22:23:51 +0100

    splash screen with svg image and Get Started button

---
+++ src/lib/components/Splash.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Button from "./Button.svelte";
+</script>
+
+<div>
+  <img src="/qatar.svg" alt="World Cup Qatar 2022 logo" />
+  <Button type="cta" text="Get Started" on:Click />
+</div>
+
+<style>
+  div {
+    display: grid;
+    place-items: center;
+  }
+
+  img {
+    width: 100%;
+    max-width: 425px;
+    margin-bottom: 2rem;
+  }
+</style>
