e5821b53902a8cfaa4112083763a3bae2395d371
kgmmm <kgmofficial0@gmail.com>
2022-04-22 21:18:19 +0100

    basic NavBar component

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let title = "World Cup Predictor";
+</script>
+
+<header>
+  <div>
+    <h1>{title}</h1>
+  </div>
+</header>
+
+<style>
+  header {
+    width: 100%;
+    height: 65px;
+    background: var(--accent);
+  }
+
+  div {
+    margin: 0 auto;
+    width: 100%;
+    max-width: 475px;
+    height: 100%;
+    display: grid;
+    align-items: center;
+  }
+
+  div h1 {
+    color: var(--background);
+    font-size: var(--font-size-2);
+    font-weight: 500;
+  }
+</style>
