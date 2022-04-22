104569ab1959f6b671447031e3e03b52ba692e43
kgmmm <kgmofficial0@gmail.com>
2022-04-23 00:23:11 +0100

    Groups in app progress

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script>
-  import { progress } from "$lib/stores";
+  import { progress, groups } from "$lib/stores";
   import Splash from "$lib/components/Splash.svelte";
+  import Group from "$lib/components/Group.svelte";
 
   function stepForward() {
     $progress[1] = $progress[1] + 1;
@@ -12,4 +13,7 @@
 
 {#if $progress[1] === 0}
   <Splash on:Click={stepForward} />
+{:else if $progress[1] > 0 && $progress[1] <= 8}
+  <p>Drag and drop the teams in the order you think the group will finish.</p>
+  <Group teams={$groups[$progress[1] - 1]} />
 {/if}
