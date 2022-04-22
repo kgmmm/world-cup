78c6bf0c46d597fc72acac1b50b64d39d79f8e0e
kgmmm <kgmofficial0@gmail.com>
2022-04-22 20:01:52 +0100

    Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let type = "normal";
+  export let text = "Button";
+</script>
+
+<button class={type}>
+  {text}
+</button>
+
+<style>
+  button {
+    width: auto;
+    height: 50px;
+    padding-inline: var(--font-size-4);
+    font-size: var(--font-size-5);
+    line-height: 100%;
+    border-radius: 50vw;
+    display: grid;
+    place-items: center;
+    cursor: pointer;
+  }
+
+  button.normal {
+    color: var(--color);
+    background: var(--background);
+    border: solid 1px var(--border);
+    font-weight: 400;
+  }
+  button.normal:hover {
+    border-color: var(--color);
+  }
+
+  button.cta {
+    background: var(--accent);
+    color: var(--background);
+    font-weight: 500;
+    border: none;
+  }
+</style>
