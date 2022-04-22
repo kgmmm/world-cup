c84b445ad76ecd3cd4a1cd2cb016246777e4c72e
kgmmm <kgmofficial0@gmail.com>
2022-04-22 22:25:08 +0100

    basic layout file

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { progress } from "$lib/stores";
+  import NavBar from "$lib/components/NavBar.svelte";
+  import Arrows from "$lib/components/Arrows.svelte";
+</script>
+
+<NavBar />
+<div class="container">
+  <slot />
+  {#if $progress[1] > 0 && $progress[1] <= 23}
+    <Arrows />
+  {/if}
+</div>
+
+<style>
+  div.container {
+    margin: 0 auto;
+    padding: 1rem;
+    width: 100%;
+    max-width: 475px;
+  }
+</style>
