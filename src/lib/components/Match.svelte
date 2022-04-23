d9f9a9d8411becbfe4070f3c7120391613fda3b3
kgmmm <kgmofficial0@gmail.com>
2022-04-23 16:36:11 +0100

    Match component that takes in two teams and saves the winner you pick to matchWinners store

---
+++ src/lib/components/Match.svelte
@@ -0,0 +1,127 @@
+<script>
+  import { codeMap, progress, matchWinners } from "$lib/stores";
+
+  export let teams;
+
+  $: teamOneCode = $codeMap.get(teams[0]).toLowerCase();
+  $: teamTwoCode = $codeMap.get(teams[1]).toLowerCase();
+</script>
+
+<div>
+  <input
+    type="radio"
+    id="teamOne"
+    name="match"
+    checked={$matchWinners[$progress[1] - 9] === teams[0]}
+  />
+  <label
+    for="teamOne"
+    on:click={() => {
+      $matchWinners[$progress[1] - 9] = teams[0];
+    }}
+  >
+    {#if teamOneCode === "tbd"}
+      <img
+        src={`https://flagcdn.com/60x45/eu.png`}
+        srcset={`https://flagcdn.com/120x90/eu.png 2x,
+        https://flagcdn.com/180x135/eu.png 3x`}
+        width="60"
+        height="45"
+        alt={teams[0]}
+      />
+    {:else}
+      <img
+        src={`https://flagcdn.com/60x45/${teamOneCode}.png`}
+        srcset={`https://flagcdn.com/120x90/${teamOneCode}.png 2x,
+        https://flagcdn.com/180x135/${teamOneCode}.png 3x`}
+        width="60"
+        height="45"
+        alt={teams[0]}
+      />
+    {/if}
+    <p>{teams[0]}</p>
+  </label>
+
+  <input
+    type="radio"
+    id="teamTwo"
+    name="match"
+    checked={$matchWinners[$progress[1] - 9] === teams[1]}
+  />
+  <label
+    for="teamTwo"
+    on:click={() => {
+      $matchWinners[$progress[1] - 9] = teams[1];
+    }}
+  >
+    {#if teamTwoCode === "tbd"}
+      <img
+        src={`https://flagcdn.com/60x45/eu.png`}
+        srcset={`https://flagcdn.com/120x90/eu.png 2x,
+        https://flagcdn.com/180x135/eu.png 3x`}
+        width="60"
+        height="45"
+        alt={teams[1]}
+      />
+    {:else}
+      <img
+        src={`https://flagcdn.com/60x45/${teamTwoCode}.png`}
+        srcset={`https://flagcdn.com/120x90/${teamTwoCode}.png 2x,
+        https://flagcdn.com/180x135/${teamTwoCode}.png 3x`}
+        width="60"
+        height="45"
+        alt={teams[1]}
+      />
+    {/if}
+    <p>{teams[1]}</p>
+  </label>
+</div>
+
+<style>
+  div {
+    margin: 1rem 0.5rem;
+    padding: 0;
+    width: 300px;
+    list-style-type: none;
+    border: solid 1px var(--border);
+    border-radius: 3px;
+    box-shadow: var(--shadow-large);
+    overflow: hidden;
+  }
+
+  input[type="radio"] {
+    display: none;
+  }
+  input[type="radio"]:checked + label {
+    color: var(--background);
+    background: var(--accent);
+  }
+
+  label {
+    padding: 1rem 0.5rem;
+    border-bottom: solid 1px var(--border);
+    background: var(--background);
+    display: grid;
+    grid-auto-flow: column;
+    align-items: center;
+    grid-template-columns: 4rem 1fr;
+    font-size: var(--font-size-5);
+    user-select: none;
+    cursor: pointer;
+    position: relative;
+  }
+  label:last-of-type {
+    border: none;
+  }
+
+  label img {
+    place-self: center;
+    pointer-events: none;
+    width: auto;
+    height: var(--font-size-2);
+  }
+
+  label p {
+    padding-left: 0.5rem;
+  }
+</style>
