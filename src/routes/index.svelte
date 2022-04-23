c9c22c55d3c553e41cb176ea083013ee76bc2a0c
kgmmm <kgmofficial0@gmail.com>
2022-04-23 16:37:46 +0100

    Group component now gets group data from store instead of props, added Match component and passing props from matches store

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,8 @@
 <script>
-  import { progress, groups } from "$lib/stores";
+  import { progress, matches } from "$lib/stores";
   import Splash from "$lib/components/Splash.svelte";
   import Group from "$lib/components/Group.svelte";
+  import Match from "$lib/components/Match.svelte";
 
   function stepForward() {
     $progress[1] = $progress[1] + 1;
@@ -15,5 +16,15 @@
   <Splash on:Click={stepForward} />
 {:else if $progress[1] > 0 && $progress[1] <= 8}
   <p>Drag and drop the teams in the order you think the group will finish.</p>
-  <Group teams={$groups[$progress[1] - 1]} />
+  <Group />
+{:else if $progress[1] > 8 && $progress[1] !== $progress[0].length - 1}
+  <p>Select the team you think will win this match.</p>
+  <Match teams={$matches[$progress[0][$progress[1]]]} />
 {/if}
+
+<style>
+  p {
+    justify-self: left;
+    text-align: left;
+  }
+</style>
