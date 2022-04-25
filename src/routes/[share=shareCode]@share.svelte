eb673cd0f049db881c8c8470bf1eb341e5e2a5af
kgmmm <kgmofficial0@gmail.com>
2022-04-25 23:48:55 +0100

    new route for shareCodes that using param validation and new blank share layout file

---
+++ src/routes/[share=shareCode]@share.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  import { decode } from "$lib/share";
+
+  export async function load({ url }) {
+    let shareCode = url.pathname.substring(1, url.pathname.length);
+
+    let data = decode(shareCode);
+
+    return {
+      props: {
+        groups: data[0],
+        matchWinners: data[1],
+      },
+    };
+  }
+</script>
+
+<script>
+  export let groups = [];
+  export let matchWinners = [];
+
+  $: console.log(groups);
+  $: console.log(matchWinners);
+</script>
