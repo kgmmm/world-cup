88d107e0d8e714019e04f19c0cdbc19b11f74ebd
kgmmm <kgmofficial0@gmail.com>
2022-04-27 00:57:47 +0100

    error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,45 @@
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: {
+        status,
+        error,
+        message: error.message,
+      },
+    };
+  }
+</script>
+
+<script>
+  import LinkButton from "$lib/components/LinkButton.svelte";
+
+  export let status;
+  export let error;
+  export let message;
+</script>
+
+<div>
+  <h1>{status}</h1>
+  <p>{message}</p>
+  <h5>Seems like that share code was invalid</h5>
+
+  <LinkButton text="Back to safety" href="/" />
+</div>
+
+<style>
+  div {
+    display: grid;
+    place-items: center;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: var(--font-size-1);
+  }
+  p {
+    margin-block: 1rem;
+  }
+  h5 {
+    font-size: var(--font-size-5);
+  }
+</style>
